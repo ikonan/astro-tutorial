102c33962b1cecc071da76cf30e436d894f70d21
ikonan <2321514568@qq.com>
2023-08-09 18:16:08 +0800

    ✨ feat: fix: ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ikonan.github.io",
   base: "/astro-tutorial",
+
+  adapter: nodejs({ mode: "standalone" }),
+  output: "hybrid",
 });
